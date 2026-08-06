77f807a0ebd821212ab5f343220c6c5420521304
Luis <luisdimeo16@gmail.com>
2026-08-05 22:12:30 -0400

    feat(matriz): completar la interfaz e implementacion del modulo GraphMatrix

--- parte_dimeo/GraphMatrix.cpp
+++ parte_dimeo/GraphMatrix.cpp
@@ -14,4 +14,34 @@ namespace SimpleGraph {
         } 
         return -1; 
     } 
+ template <typename T> 
+    void GraphMatrix<T>::addVertex(const T& vertex) { 
+        if (getVertexIndex(vertex) == -1) { 
+            vertices.push_back(vertex); 
+            int newSize = vertices.size(); 
+             
+            for (int i = 0; i < matrix.size(); i++) { 
+                matrix.at(i).push_back(999999.0); 
+            } 
+             
+            CustomVector<double> newRow; 
+            newRow.resize(newSize, 999999.0); 
+            matrix.push_back(newRow); 
+        } 
+    } 
  
+    template <typename T> 
+    void GraphMatrix<T>::addEdge(const T& source, const T& destination, double weight) { 
+        addVertex(source); 
+        addVertex(destination); 
+ 
+        int srcIdx = getVertexIndex(source); 
+        int destIdx = getVertexIndex(destination); 
+ 
+        matrix.at(srcIdx).at(destIdx) = weight; 
+        if (!isDirected) { 
+            matrix.at(destIdx).at(srcIdx) = weight; 
+        } 
+    } 
+} 
+#endif 
